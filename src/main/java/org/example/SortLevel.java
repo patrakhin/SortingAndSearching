d0e0208eb7ff181015198092233cdff4cfd093fb
patrakhin <patrakhin@mail.ru>
2023-09-10 19:01:27 +0300

    added Solving SortLevel

---
+++ src/main/java/org/example/SortLevel.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.*;
+
+public class SortLevel {
+    public static int[] SelectionSortStep(int[] array, int i)
+    {
+        int number = array[i];
+        int index = 0;
+        int buff = 0;
+        int min = array[i+1];
+        if (i == array.length-1){
+            return array;
+        }
+        for (int j = i +1; j < array.length; j++){
+            if (min >array[j]){
+                min = array[j];
+                index = j;
+            }
+        }
+        if (number > min){
+            buff = number;
+            array[i] = min;
+            array[index] = buff;
+        }
+        return array ;
+    }
+
+    public static boolean BubbleSortStep( int[] array ){
+
+        int changeBox = 0;
+        boolean isChangeDone = true;
+        for (int i = 0; i < array.length - 1; i++){
+            if (array[i] > array[i+1]){
+                changeBox = array[i];
+                array[i] = array[i+1];
+                array[i+1] = changeBox;
+                isChangeDone = false;
+            }
+        }
+        return isChangeDone;
+    }
+}
---
+++ src/main/java/org/example/SortLevelForSolving.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SortLevelForSolving {
+    public static int[] SelectionSortStep(int[] array, int i)
+    {
+        int number = array[i];
+        int index = 0;
+        int buff = 0;
+        int min = array[i+1];
+        if (i == array.length-1){
+            return array;
+        }
+        for (int j = i +1; j < array.length; j++){
+            if (min >array[j]){
+                min = array[j];
+                index = j;
+            }
+        }
+        if (number > min){
+            buff = number;
+            array[i] = min;
+            array[index] = buff;
+        }
+        return array ;
+    }
+
+    public static boolean BubbleSortStep( int[] array ){
+
+        int changeBox = 0;
+        boolean isChangeDone = true;
+        for (int i = 0; i < array.length - 1; i++){
+            if (array[i] > array[i+1]){
+                changeBox = array[i];
+                array[i] = array[i+1];
+                array[i+1] = changeBox;
+                isChangeDone = false;
+            }
+        }
+        return isChangeDone;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {2, 3, 5, 4, 7, 6, 8, 9, 1};
+        int numberIndex = 3;
+        System.out.println(Arrays.toString(SelectionSortStep(array, numberIndex)));
+        int[] boolArray = {1, 1, 2, 1, 1, 1, 1, 1};
+        System.out.println(BubbleSortStep(boolArray));
+    }
+}
+
+
