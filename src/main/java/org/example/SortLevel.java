5b88e10bc5f4710bb62c950397a14bb4a2508d53
patrakhin <patrakhin@mail.ru>
2023-09-12 21:59:43 +0300

    added fix 4 to SortL_002

--- src/main/java/org/example/SortLevel.java
+++ src/main/java/org/example/SortLevel.java
@@ -6,24 +6,22 @@ public class SortLevel {
     public static int[] InsertionSortStep (int[] array, int step, int i) {
         int different = array.length - i;
         int sizeBuffArray = 0;
-        for (int a = 0; a < 1; a ++) {
-            if ((different- a) % step == 0){
-                sizeBuffArray = (different - a) / step;
-            }else{
-                sizeBuffArray = (different - a) / step + 1;
-            }
-            int[] buffArray = new int[sizeBuffArray];
-            int[] indexArray = new int[sizeBuffArray];
-            for (int j = i, k = 0; j < different; j +=step, k++){
-                buffArray[k] = array[j];
-                indexArray[k] = j;
-            }
-            Arrays.sort(buffArray);
-            for (int x = 0; x < buffArray.length; x++){
-                int insertIndex = indexArray[x];
-                int numberOfArray = buffArray[x];
-                array[insertIndex] = numberOfArray;
-            }
+        if ((different) % step == 0){
+            sizeBuffArray = (different) / step;
+        }else{
+            sizeBuffArray = (different) / step + 1;
+        }
+        int[] buffArray = new int[sizeBuffArray];
+        int[] indexArray = new int[sizeBuffArray];
+        for (int j = i, k = 0; j < array.length; j +=step, k++){
+            buffArray[k] = array[j];
+            indexArray[k] = j;
+        }
+        Arrays.sort(buffArray);
+        for (int x = 0; x < buffArray.length; x++){
+            int insertIndex = indexArray[x];
+            int numberOfArray = buffArray[x];
+            array[insertIndex] = numberOfArray;
         }
         return array;
     }
--- src/main/java/org/example/SortLevelForSolving_002.java
+++ src/main/java/org/example/SortLevelForSolving_002.java
@@ -6,33 +6,31 @@ public class SortLevelForSolving_002 {
     public static int[] InsertionSortStep (int[] array, int step, int i) {
         int different = array.length - i;
         int sizeBuffArray = 0;
-        for (int a = 0; a < 1; a ++) {
-            if ((different- a) % step == 0){
-                sizeBuffArray = (different - a) / step;
-            }else{
-                sizeBuffArray = (different - a) / step + 1;
-            }
-            int[] buffArray = new int[sizeBuffArray];
-            int[] indexArray = new int[sizeBuffArray];
-            for (int j = i, k = 0; j < different; j +=step, k++){
-                buffArray[k] = array[j];
-                indexArray[k] = j;
-            }
-            Arrays.sort(buffArray);
-            for (int x = 0; x < buffArray.length; x++){
-                int insertIndex = indexArray[x];
-                int numberOfArray = buffArray[x];
-                array[insertIndex] = numberOfArray;
-            }
+        if ((different) % step == 0){
+            sizeBuffArray = (different) / step;
+        }else{
+            sizeBuffArray = (different) / step + 1;
+        }
+        int[] buffArray = new int[sizeBuffArray];
+        int[] indexArray = new int[sizeBuffArray];
+        for (int j = i, k = 0; j < array.length; j +=step, k++){
+            buffArray[k] = array[j];
+            indexArray[k] = j;
+        }
+        Arrays.sort(buffArray);
+        for (int x = 0; x < buffArray.length; x++){
+            int insertIndex = indexArray[x];
+            int numberOfArray = buffArray[x];
+            array[insertIndex] = numberOfArray;
         }
         return array;
     }
 
     public static void main(String[] args) {
 
-        int[] array = {7,6,5,4,3,2,1 };
+        int[] array = {1, 6, 5, 4, 3, 2, 7};
         int step = 3;
-        int i =0;
+        int i =1;
         System.out.println(Arrays.toString(InsertionSortStep(array,step, i)));
     }
 
