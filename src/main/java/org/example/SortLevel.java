62ae44760dfa2dd8bacad3d9592ba26be3b8ef4f
patrakhin <patrakhin@mail.ru>
2023-09-15 19:46:08 +0300

    added fix 3 to Shell Sort 150923

--- src/main/java/org/example/SortLevel.java
+++ src/main/java/org/example/SortLevel.java
@@ -5,10 +5,14 @@ import java.util.*;
 public class SortLevel {
 
     public static ArrayList<Integer> KnuthSequence(int array_size){
-        int h = 1;
         ArrayList<Integer> step = new ArrayList<>();
-        while (h < array_size || h == 1){
-            step.add(h);
+        int h = 1;
+        if (array_size == 1){
+            step.add(0, 1);
+            return step;
+        }
+        while (h < array_size){
+            step.add(0, h);
             h = 3 * h + 1;
         }
         return step;
--- src/main/java/org/example/SortLevelForSolving_003.java
+++ src/main/java/org/example/SortLevelForSolving_003.java
@@ -7,10 +7,14 @@ import java.util.List;
 
 public class SortLevelForSolving_003 {
     public static ArrayList<Integer> KnuthSequence(int array_size){
-        int h = 1;
         ArrayList<Integer> step = new ArrayList<>();
-        while (h < array_size || h == 1){
-            step.add(h);
+        int h = 1;
+        if (array_size == 1){
+            step.add(0, 1);
+            return step;
+        }
+        while (h < array_size){
+            step.add(0, h);
             h = 3 * h + 1;
         }
         return step;
@@ -19,7 +23,7 @@ public class SortLevelForSolving_003 {
     public static void main(String[] args) {
 
         int[] array = {1, 6, 5, 4, 3, 2, 7};
-        int d = 15;
+        int d = -1;
         System.out.println(KnuthSequence(d));
     }
 }
