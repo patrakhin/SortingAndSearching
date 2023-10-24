5f6c214eff6f9695bd95d4444ffad9ec6ca398f3
andrey <patrakhin@mail.ru>
2023-10-24 12:37:00 +0300

    added sol BinarySearch 123924102023

---
+++ src/main/java/org/example/BinarySearch.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class BinarySearch {
+    private int[] array;
+    public int Left;
+    public int Right;
+    private int result;
+
+    public BinarySearch(int[] sortedArray) {
+        array = sortedArray;
+        Left = 0;
+        Right = array.length - 1;
+        result = 0;
+    }
+
+    public void Step(int N) {
+        if (Left > Right || result != 0) return;
+
+        int mid = (Left + Right) / 2;
+
+        if (array[mid] == N) {
+            result = 1;
+        } else if (array[mid] < N) {
+            Left = mid + 1;
+        } else {
+            Right = mid - 1;
+        }
+
+        if (Left == Right || Left > Right) {
+            result = -1;
+        }
+    }
+
+    public int GetResult() {
+        return result;
+    }
+}
+
