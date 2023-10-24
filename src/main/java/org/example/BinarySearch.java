c3bdc9cfc9084937bda32a7e20395e9f81dc7d82
andrey <patrakhin@mail.ru>
2023-10-24 20:45:47 +0300

    added fix6 BinarySearch 162524102023

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class BinarySearch {
-    public int[] array;
+    private int[] array;
     public int Left;
     public int Right;
-    public int result;
+    private int result;
 
     public BinarySearch(int[] sortedArray) {
         array = sortedArray;
@@ -14,28 +14,28 @@ public class BinarySearch {
     }
 
     public void Step(int N) {
+        if (Left > Right || result != 0) return;
 
         int mid = (Left + Right) / 2;
-        if (Left == Right || (Left != Right && (Math.abs(Left - Right) == 1))) {
-            result = 1;
-            return;
-        }
 
         if (array[mid] == N) {
             result = 1;
             return;
-        }
-        if (N > array[mid]) {
+        } else if (N > array[mid]) {
             Left = mid + 1;
-        }
-        if (N < array[mid]){
+        } else {
             Right = mid - 1;
         }
 
+        if (Left == Right || Math.abs(Left - Right) == 1) {
+            result = 0;
+        }
     }
 
     public int GetResult() {
-        return result;
+        int tempResult = result;
+        result = 0;
+        return tempResult;
     }
 
 }
