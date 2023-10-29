19eaf7ca82512c185372d25608b49026557eea00
patrakhin <patrakhin@mail.ru>
2023-10-29 19:35:11 +0300

    added fix4 SortLevel 193529102023

--- src/main/java/org/example/SortLevel.java
+++ src/main/java/org/example/SortLevel.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class SortLevel {
 
-
     public static boolean GallopingSearch(int[] array, int target) {
         int i = 1;
         int currentIndex = (int) (Math.pow(2, i) - 2);
@@ -37,35 +36,36 @@ public class SortLevel {
         }
         return false;
     }
+
+   public static class BinarySearch {
+        public int[] array;
+        public int Left;
+        public int Right;
+        public int result;
+
+        public BinarySearch(int[] sortedArray, int left, int right) {
+            array = sortedArray;
+            Left = left;
+            Right = right;
+            result = 0;
+        }
+
+        public void Step(int N) {
+            int mid = (Left + Right) / 2;
+            if (array[mid] == N) {
+                result = 1;
+                return;
+            }
+            if (N > array[mid]) {
+                Left = mid + 1;
+            } else {
+                Right = mid - 1;
+            }
+        }
+
+        public int GetResult() {
+            return result;
+        }
+    }
 }
 
-class BinarySearch {
-    private int[] array;
-    public int Left;
-    public int Right;
-    private int result;
-
-    public BinarySearch(int[] sortedArray, int left, int right) {
-        array = sortedArray;
-        Left = left;
-        Right = right;
-        result = 0;
-    }
-
-    public void Step(int N) {
-        int mid = (Left + Right) / 2;
-        if (array[mid] == N) {
-            result = 1;
-            return;
-        }
-        if (N > array[mid]) {
-            Left = mid + 1;
-        } else {
-            Right = mid - 1;
-        }
-    }
-
-    public int GetResult() {
-        return result;
-    }
-}
